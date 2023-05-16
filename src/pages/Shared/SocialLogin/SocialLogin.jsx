1f4fcfec0f2b2d2f58e3f9ad62798f3b00ffb5cb
tanjumtanha <tanha.tanni27@gmail.com>
2023-05-16 18:35:29 +0600

    car doctor client site design

---
+++ src/pages/Shared/SocialLogin/SocialLogin.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useContext } from "react";
+import { AuthContext } from "../../../providers/AuthProvider";
+
+const SocialLogin = () => {
+    const { googleSignIn } = useContext(AuthContext);
+    const handleGoogleSignIn = () => {
+        googleSignIn()
+            .then(result => {
+                console.log(result.user);
+            })
+            .catch(error => console.log(error))
+    }
+
+    return (
+        <div>
+            <div className="divider">OR</div>
+            <div className="text-center">
+                <button onClick={handleGoogleSignIn} className="btn btn-outline">
+                    Google
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default SocialLogin;
